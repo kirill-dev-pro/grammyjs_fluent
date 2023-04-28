6def68391b578e154cf51d6eb3f67bdacc5854c1
Kirill <8945959+kirill-dev-pro@users.noreply.github.com>
2023-04-28 23:07:58 +0300

    Update grammy to version 1.16.0

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export { Context } from "https://deno.land/x/grammy@v1.10.1/mod.ts";
+export { Context } from "https://deno.land/x/grammy@v1.16.0/mod.ts";
 export type {
   Middleware,
   NextFunction,
   SessionFlavor,
   StorageAdapter,
-} from "https://deno.land/x/grammy@v1.10.1/mod.ts";
+} from "https://deno.land/x/grammy@v1.16.0/mod.ts";
 export { Fluent } from "https://deno.land/x/better_fluent@v1.0.0/mod.ts";
 export type {
   LocaleId,
